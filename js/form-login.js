cccea48c6eedbf9d8622cbc1c957df0243507631
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-03-25 16:09:41 +0100

    Form validation: variables created

--- js/form-login.js
+++ js/form-login.js
@@ -1,5 +1,14 @@
+const formContainer = document.querySelector("#form");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#email-error");
+const password = document.querySelector("#password");
+const passwordError = document.querySelector("#password-error");
 const loginButton = document.querySelector("#login");
 
-loginButton.style.display = "none";
 
-console.log("Test");
+/* passwordError.style.display = "none"; */
+/* emailError.style.display = "none"; */
+/* formContainer.style.display = "none" */
+/* loginButton.style.display = "none" */
+
+
