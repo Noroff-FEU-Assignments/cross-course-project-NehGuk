d3ecf82c286ce76d81f02365c89d25c638270174
Neh Guk <84685506+NehGuk@users.noreply.github.com>
2022-04-24 00:23:24 +0200

    Search results page: added loader

--- js/search-results.js
+++ js/search-results.js
@@ -2,12 +2,15 @@ const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 const urlTagSearch = `https://henrikugler.no/cmscaapi/wp-json/wc/v3/products?consumer_key=ck_4ee7eb518ecca82639976d69725b240eca7cd1e5&consumer_secret=cs_3ab719e026712e212e3eeefdb0f8a25ec4c3326d&per_page=90&tag=${id}`;
 const topRatedContainer = document.querySelector(".top-rated-container");
+const loader = document.querySelector(".loader");
 
 async function getTagSearch() {
   try {
     const res = await fetch(urlTagSearch);
     const films = await res.json();
 
+
+    loader.style.display = "none";
     topRatedContainer.innerHTML = "";
 
     for (let i = 0; i < films.length; i++) {
